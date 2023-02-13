86bcc52892fe4ee186feb38f4f8fcef68e29ca84
adoughty16 <adoughty16@gmail.com>
2023-02-13 17:12:09 -0500

    tip toes done

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import pyrosim.pyrosim as pyrosim
 import time
 import pybullet_data
 import numpy
+import math
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,13 +22,27 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-frontLegSensorValues = numpy.zeros(200)
-torsoSensorValues = numpy.zeros(200)
+frontLegSensorValues = numpy.zeros(1000)
+torsoSensorValues = numpy.zeros(1000)
 
-for i in range(200):
+for i in range(1000):
 	p.stepSimulation()
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 	torsoSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Torso")
+
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robotId,
+		jointName = b"BackLeg_Torso",
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = math.pi/4.0,
+		maxForce = 500)
+
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robotId,
+		jointName = b"Torso_FrontLeg",
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = math.pi/4.0,
+		maxForce = 500)
 
 	time.sleep(1/60)
 p.disconnect()
